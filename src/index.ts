6cb7d0cee9dbc013970efbd6ac40ed156319244c
Sooji Kim <yh0gogo@gmail.com>
2020-03-06 01:28:02 +0900

    Add up class in the ts sample code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "suzie",
-  age: 20,
-  gender: "female"
-};
+const suzie = new Human("suzie", 20, "female");
 
 const sayHi = (person: Human): string => {
   return `hello ${person.name}, you are ${person.age} years old, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(suzie));
 
 export {};
