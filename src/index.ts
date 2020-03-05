82ad36a4680eb3d50102085d023f5638c861c348
Sooji Kim <yh0gogo@gmail.com>
2020-03-06 01:18:59 +0900

    Apply interface on the sample code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `hello ${name}, you are ${age} years old, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "suzie",
+  age: 20,
+  gender: "female"
 };
 
-console.log(sayHi("suzie", 20, "female"));
+const sayHi = (person: Human): string => {
+  return `hello ${person.name}, you are ${person.age} years old, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
